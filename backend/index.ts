7f84b4b16e8554cebce412cf34a98922451c9408
Avinash Yadav <avinashyadav4774@gmail.com>
2026-06-29 23:31:44 +0530

    generated prisma client and test submission endpoint

--- backend/index.ts
+++ backend/index.ts
@@ -2,24 +2,27 @@ import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client/extension";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { randomBytes, randomUUID }from "crypto";
 
 const app =  express();
 
-const prisma =  new PrismaClient(
-    {
-        adapter: new PrismaPg({ connectionString : process.env.DATABASE_URL });
-    }
-)
+const prisma  = new PrismaClient({
+    adapter: new PrismaPg({ connectionString : process.env.DATABASE_URL  })
+})
+
 app.use( express.json() );
 app.use(cors());
 
-app.post("/submission" ,( req, res ) =>{
+app.post("/submission" ,async ( req, res ) =>{
+     const id  = randomUUID();
     await prisma.submission.create({
         data:{
-            
+            submissionId : id,
+            status:"PROCESSING",
+
         }
     })
-    return res.status(200).json({ message: " processing "});
+    return res.status(200).json({ message: " processing ", id:id });
 })
 
 
