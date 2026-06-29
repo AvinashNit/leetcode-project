5806795659b6e5e89dd99c48bd6394cf32a461ee
Avinash Yadav <avinashyadav4774@gmail.com>
2026-06-29 23:55:12 +0530

    connected to redis

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { randomBytes, randomUUID }from "crypto";
+import { createClient } from "redis";
 
 const app =  express();
 
@@ -10,10 +11,24 @@ const prisma  = new PrismaClient({
     adapter: new PrismaPg({ connectionString : process.env.DATABASE_URL  })
 })
 
+const publisher  =  createClient({
+    url: process.env.REDIS_URL
+})
+
+publisher.on("error",(err)=>{
+    console.log(" redis client error ", err)
+
+})
+
+await publisher.connect();
+console.log(" connecte to reds ") 
+
+
 app.use( express.json() );
 app.use(cors());
 
 app.post("/submission" ,async ( req, res ) =>{
+    const { language, code } = req.body;
      const id  = randomUUID();
     await prisma.submission.create({
         data:{
